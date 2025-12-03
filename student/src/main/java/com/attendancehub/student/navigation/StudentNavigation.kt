b621194e88b1bd169b3403e2312b490482d36411
Wahid Ali <1213089@dtu.edu.eg>
2025-12-03 15:45:21 +0200

    Add student information management to ViewModel and navigation

--- student/src/main/java/com/attendancehub/student/navigation/StudentNavigation.kt
+++ student/src/main/java/com/attendancehub/student/navigation/StudentNavigation.kt
@@ -2,6 +2,7 @@ package com.attendancehub.student.navigation
 
 sealed class StudentScreen(val route: String) {
     object Permissions : StudentScreen("permissions")
+    object StudentInfo : StudentScreen("student_info")
     object NetworkScan : StudentScreen("network_scan")
     object QRScanner : StudentScreen("qr_scanner")
     object Connecting : StudentScreen("connecting/{networkName}") {
